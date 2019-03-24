6058522102074e7216ea72d8c9317f6c04d9586f
unknown <burkeamy30@gmail.com>
2019-03-24 15:47:13 -0400

    things are starting to come together

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -12,7 +12,7 @@ router.get("/", function(req, res) {
       burgers: data
     };
     console.log(hbsObject);
-    res.render("index", hbsObject);
+    res.render("./partials/index", hbsObject);
   });
 });
 
--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,11 @@
 DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
+
+CREATE TABLE burger
+(
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
