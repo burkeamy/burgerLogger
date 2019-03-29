ecf069a2ad19f19945f9d5d194d8769b90923672
unknown <burkeamy30@gmail.com>
2019-03-28 20:28:35 -0400

    Adds jawsdb connection variable

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
+let connection;
 
-const connection = mysql.createConnection({
-  host: "127.0.0.1",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+// Set up MySQL connection for deployment on Heroku
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: '127.0.0.1',
+    port: "3306",
+    user: 'root',
+    password: 'root',
+    database: 'burgers_db'
+  });
+}
 
 // Make connection.
 connection.connect(err => {
